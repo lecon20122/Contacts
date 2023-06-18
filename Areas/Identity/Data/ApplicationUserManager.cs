18d6133cb2def0308943cfee5c8943c396d7f604
lecon20122 <lecon20122@gmail.com>
2023-06-18 18:58:40 +0300

    Adding ApplicationUserManager, parsing GetUserId result to Int

---
+++ Areas/Identity/Data/ApplicationUserManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace Contacts.Areas.Identity.Data
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<ApplicationUser> passwordHasher, IEnumerable<IUserValidator<ApplicationUser>> userValidators, IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<ApplicationUser>> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        {
+        }
+
+        public int UserId(ClaimsPrincipal user)
+        {
+            return int.Parse(GetUserId(user));
+        }
+    }
+}
